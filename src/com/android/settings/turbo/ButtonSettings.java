1b81d52a0ed39e4aa62e6d6e64b40a60eb8c0ca8
Muhammed Nazim <mnm9994u@gmail.com>
2014-01-11 23:27:20 +1100

    Settings: Add advanced reboot [2/2]
    Change-Id: I59690daf92514dff10543171bb3eba435fa216e5
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/turbo/ButtonSettings.java
+++ src/com/android/settings/turbo/ButtonSettings.java
@@ -19,8 +19,11 @@ package com.android.settings.turbo;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.provider.Settings;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.view.IWindowManager;
 
@@ -28,15 +31,28 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class ButtonSettings extends SettingsPreferenceFragment {
+public class ButtonSettings extends SettingsPreferenceFragment
+       implements OnPreferenceChangeListener {
 
     private static final String KEY_HARDWARE_KEYS = "hardwarekeys_settings";
+    private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
+
+    private ListPreference mAdvancedReboot;
+
+    private boolean mIsPrimary;
+    private static final int MY_USER_ID = UserHandle.myUserId();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        PreferenceScreen root = getPreferenceScreen();
+        if (root != null) {
+            root.removeAll();
+        }
+
         addPreferencesFromResource(R.xml.button_settings);
+        root = getPreferenceScreen();
         
         // Hide Hardware Keys menu if device doesn't have any
         PreferenceScreen hardwareKeys = (PreferenceScreen) findPreference(KEY_HARDWARE_KEYS);
@@ -45,10 +61,37 @@ public class ButtonSettings extends SettingsPreferenceFragment {
         if (deviceKeys == 0 && hardwareKeys != null) {
             getPreferenceScreen().removePreference(hardwareKeys);
         }
+
+        mIsPrimary = MY_USER_ID == UserHandle.USER_OWNER;
+
+        mAdvancedReboot = (ListPreference) root.findPreference(KEY_ADVANCED_REBOOT);
+        if (mIsPrimary) {
+            mAdvancedReboot.setValue(String.valueOf(Settings.Secure.getInt(
+                    getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0)));
+            mAdvancedReboot.setOnPreferenceChangeListener(this);
+        } else {
+            root.removePreference(mAdvancedReboot);
+        }
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+ 	boolean result = true;
+        if (preference == mAdvancedReboot) {
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.ADVANCED_REBOOT,
+                Integer.valueOf((String) value));
+            mAdvancedReboot.setValue(String.valueOf(value));
+        }
+        return result;
     }
 }
