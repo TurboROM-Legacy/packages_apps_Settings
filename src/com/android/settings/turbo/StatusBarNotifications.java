856e6a568d68da06c17d8b7ac81847244d56c533
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-11-04 03:46:47 +0100

    Custom status bar notification icon color (2/2)
    - Icons color
    - icons color, (dark mode)

--- src/com/android/settings/turbo/StatusBarNotifications.java
+++ src/com/android/settings/turbo/StatusBarNotifications.java
@@ -16,8 +16,13 @@
 
 package com.android.settings.turbo;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -29,6 +34,9 @@ import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -36,31 +44,49 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.Utils;
 import com.android.internal.util.slim.DeviceUtils;
 
-public class StatusBarNotifications extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class StatusBarNotifications extends SettingsPreferenceFragment 
+	implements Preference.OnPreferenceChangeListener {
 
     private static final String BREATHING_NOTIFICATIONS = "breathing_notifications";
     private static final String MISSED_CALL_BREATH = "missed_call_breath";
     private static final String SMS_BREATH = "sms_breath";
     private static final String VOICEMAIL_BREATH = "voicemail_breath";
+    private static final String PREF_CAT_COLORS = "notification_cat_colors";
+    private static final String PREF_ICON_COLOR = "notification_icon_color";
+    private static final String PREF_ICON_COLOR_DARK = "notification_icon_color_dark_mode";
 
     private PreferenceGroup mBreathingNotifications;
     private SwitchPreference mMissedCallBreath;
     private SwitchPreference mSmsBreath;
     private SwitchPreference mVoicemailBreath;
+    private ColorPickerPreference mIconColor;
+    private ColorPickerPreference mIconColorDark;
 
-    @Override
-    protected int getMetricsCategory() {
-       return MetricsLogger.DONT_TRACK_ME_BRO;
-    }
+    private static final int WHITE           = 0xffffffff;
+    private static final int BLACK           = 0xff000000;
+    private static final int HOLO_BLUE_LIGHT = 0xff33b5e5;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET  = 0;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.statusbar_notifications);
-        PreferenceScreen prefSet = getPreferenceScreen();
-
-        final ContentResolver resolver = getActivity().getContentResolver();
+ 	mResolver = getActivity().getContentResolver();
 
         mBreathingNotifications = (PreferenceGroup) findPreference(BREATHING_NOTIFICATIONS);
         mMissedCallBreath = (SwitchPreference) findPreference(MISSED_CALL_BREATH);
@@ -73,22 +99,69 @@ public class StatusBarNotifications extends SettingsPreferenceFragment implement
 
         if(cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE)) {
 
-            mMissedCallBreath.setChecked(Settings.System.getInt(resolver,
+            mMissedCallBreath.setChecked(Settings.System.getInt(mResolver,
                     Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1);
             mMissedCallBreath.setOnPreferenceChangeListener(this);
 
-            mVoicemailBreath.setChecked(Settings.System.getInt(resolver,
+            mVoicemailBreath.setChecked(Settings.System.getInt(mResolver,
                     Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1);
             mVoicemailBreath.setOnPreferenceChangeListener(this);
 
-            mSmsBreath.setChecked(Settings.Global.getInt(resolver,
+            mSmsBreath.setChecked(Settings.Global.getInt(mResolver,
                     Settings.Global.KEY_SMS_BREATH, 0) == 1);
             mSmsBreath.setOnPreferenceChangeListener(this);
         } else {
-            prefSet.removePreference(mMissedCallBreath);
-            prefSet.removePreference(mVoicemailBreath);
-            prefSet.removePreference(mSmsBreath);
-            prefSet.removePreference(mBreathingNotifications);
+            prefs.removePreference(mMissedCallBreath);
+            prefs.removePreference(mVoicemailBreath);
+            prefs.removePreference(mSmsBreath);
+            prefs.removePreference(mBreathingNotifications);
+        }
+
+        int intColor;
+        String hexColor;
+
+        mIconColor =
+                (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR, WHITE);
+        mIconColor.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColor.setSummary(hexColor);
+        mIconColor.setDefaultColors(WHITE, HOLO_BLUE_LIGHT);
+        mIconColor.setOnPreferenceChangeListener(this);
+
+        mIconColorDark =
+                (ColorPickerPreference) findPreference(PREF_ICON_COLOR_DARK);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR_DARK_MODE,
+                BLACK);
+        mIconColorDark.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColorDark.setSummary(hexColor);
+        mIconColorDark.setDefaultColors(BLACK, BLACK);
+        mIconColorDark.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory catColors =
+                (PreferenceCategory) findPreference(PREF_CAT_COLORS);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_action_reset)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
         }
     }
 
@@ -99,20 +172,108 @@ public class StatusBarNotifications extends SettingsPreferenceFragment implement
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String hex;
+        int intHex;
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mMissedCallBreath) {
             boolean value = (Boolean) newValue;
-            Settings.System.putInt(resolver, Settings.System.KEY_MISSED_CALL_BREATH, value ? 1 : 0);
+            Settings.System.putInt(mResolver, Settings.System.KEY_MISSED_CALL_BREATH, value ? 1 : 0);
             return true;
         } else if (preference == mVoicemailBreath) {
             boolean value = (Boolean) newValue;
-            Settings.System.putInt(resolver, Settings.System.KEY_VOICEMAIL_BREATH, value ? 1 : 0);
+            Settings.System.putInt(mResolver, Settings.System.KEY_VOICEMAIL_BREATH, value ? 1 : 0);
             return true;
         } else if (preference == mSmsBreath) {
             boolean value = (Boolean) newValue;
-            Settings.Global.putInt(resolver, Settings.Global.KEY_SMS_BREATH, value ? 1 : 0);
+            Settings.Global.putInt(mResolver, Settings.Global.KEY_SMS_BREATH, value ? 1 : 0);
+            return true;
+        } else if (preference == mIconColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mIconColorDark) {
+            hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR_DARK_MODE,
+                    intHex);
+            preference.setSummary(hex);
             return true;
         }
         return false;
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        StatusBarNotifications getOwner() {
+            return (StatusBarNotifications) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.dlg_reset_colors_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.dlg_reset_android,
+                            new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR,
+                                    WHITE);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR_DARK_MODE,
+                                    BLACK);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_reset_turbo,
+                            new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR,
+                                    HOLO_BLUE_LIGHT);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_NOTIFICATION_ICONS_COLOR_DARK_MODE,
+                                    BLACK);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+       return MetricsLogger.DONT_TRACK_ME_BRO;
     }
 }
