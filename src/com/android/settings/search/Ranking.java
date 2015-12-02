6beafa99d1061c8e2b5bf1262d02c1e772d03c0f
Raleigh Matlock <raleighman2@gmail.com>
2015-12-02 10:52:16 -0500

    Add Turbo settings category to Dashboard
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/search/Ranking.java
+++ src/com/android/settings/search/Ranking.java
@@ -47,6 +47,7 @@ import com.android.settings.notification.ZenModePrioritySettings;
 import com.android.settings.notification.ZenModeSettings;
 import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.turbo.MainSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
@@ -81,6 +82,7 @@ public final class Ranking {
     public static final int RANK_PRINTING = 20;
     public static final int RANK_DEVELOPEMENT = 21;
     public static final int RANK_DEVICE_INFO = 22;
+    public static final int RANK_TURBO = 23;
 
     public static final int RANK_UNDEFINED = -1;
     public static final int RANK_OTHERS = 1024;
@@ -113,6 +115,9 @@ public final class Ranking {
 
         // Home
         sRankMap.put(HomeSettings.class.getName(), RANK_HOME);
+
+	// Turbo
+	sRankMap.put(MainSettings.class.getName(), RANK_TURBO);
 
         // Display
         sRankMap.put(DisplaySettings.class.getName(), RANK_DISPLAY);
