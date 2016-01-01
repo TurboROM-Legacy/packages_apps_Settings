9ebcf17ecc062576aacc555641f1939b0e71888d
Danesh M <daneshm90@gmail.com>
2016-01-01 03:01:20 -0500

    Settings : Add screen timeout
    Change-Id: I146be2a154f701d3f33421cddb177f0511dea2fb

--- src/com/android/settings/turbo/qs/QSTileHolder.java
+++ src/com/android/settings/turbo/qs/QSTileHolder.java
@@ -102,6 +102,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_compass_on";
                 stringId = R.string.qs_tile_compass;
                 break;
+            case QSConstants.TILE_SCREEN_TIMEOUT:
+                resourceName = "ic_qs_screen_timeout_vector";
+                stringId = R.string.qs_tile_screen_timeout;
+                break;
             default:
                 return null;
         }
