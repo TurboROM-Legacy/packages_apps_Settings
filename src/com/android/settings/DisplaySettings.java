2a0ba18a1fd1237384b4abd9248712c81a77cb3f
Alex Cruz <mazdarider23@gmail.com>
2016-01-21 11:06:02 -0500

    Configurable dashboard columns [2/2]
    Before this, the only way to get double columns on a device like the Nexus 6
    was to adjust the config.xml and while that works for some, it doesn't for all.
    
    This method allows those that like to adjust the columns to do so while keeping
    the stock look :-)
    
    Thanks to @xWASABI for the idea in where he adjusted the config.xml
    https://github.com/TeamTwisted/packages_apps_Settings/commit/3c62ed2f244ae21436e02556894c2f5b46365024
    
    Change-Id: Id99ad00974ec5fe3aeef8e9cbabf6fe156c54bf2

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -62,6 +62,7 @@ import android.util.Log;
 
 import com.android.settings.util.Helpers;
 import com.android.settings.turbo.DisplayRotation;
+import com.android.settings.dashboard.DashboardContainerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -88,6 +89,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
+
+    private static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -117,6 +120,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mAutoBrightnessPreference;
     private SwitchPreference mCameraGesturePreference;
     private SwitchPreference mCameraDoubleTapPowerGesturePreference;
+
+    private ListPreference mDashboardColumns;
 
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
@@ -162,6 +167,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mFontSizePref = (FontDialogPreference) findPreference(KEY_FONT_SIZE);
         mFontSizePref.setOnPreferenceChangeListener(this);
         mFontSizePref.setOnPreferenceClickListener(this);
+
+        mDashboardColumns = (ListPreference) findPreference(DASHBOARD_COLUMNS);
+        mDashboardColumns.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.DASHBOARD_COLUMNS, DashboardContainerView.mDashboardValue)));
+        mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+        mDashboardColumns.setOnPreferenceChangeListener(this);
 
         if (isAutomaticBrightnessAvailable(getResources())) {
             mAutoBrightnessPreference = (SwitchPreference) findPreference(KEY_AUTO_BRIGHTNESS);
@@ -538,6 +549,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), CAMERA_GESTURE_DISABLED,
                     value ? 0 : 1 /* Backwards because setting is for disabling */);
+        }
+        if (preference == mDashboardColumns) {
+            Settings.System.putInt(getContentResolver(), Settings.System.DASHBOARD_COLUMNS,
+                    Integer.valueOf((String) objValue));
+            mDashboardColumns.setValue(String.valueOf(objValue));
+            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+            return true;
         }
         if (preference == mCameraDoubleTapPowerGesturePreference) {
             boolean value = (Boolean) objValue;
