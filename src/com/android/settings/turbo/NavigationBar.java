3e6c2dced05a7e84593c06cfb26bbf1baeba6413
Raleigh Matlock <raleighman2@gmail.com>
2016-03-21 17:35:38 -0400

    Add Navigation bar fragment to Turbo tweaks
    * Also add import of Turbo utils to local java libraries

---
+++ src/com/android/settings/turbo/NavigationBar.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016 Turbo ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package com.android.settings.turbo;
+
+import java.util.List;
+
+import android.app.ActionBar;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NavigationBar extends SettingsPreferenceFragment {
+
+    private static final String TAG = NavigationBar.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.navigation_bar);
+    }
+    
+    @Override
+        protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+}
