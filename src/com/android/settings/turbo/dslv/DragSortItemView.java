288c7558712d296f3d9f82da34269a594a194798
Raleigh Matlock <raleighman2@gmail.com>
2016-03-23 21:58:44 -0400

    Fix Slim actions & dslv (2/2)

--- src/com/android/settings/turbo/dslv/DragSortItemView.java
+++ src/com/android/settings/turbo/dslv/DragSortItemView.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.slim.dslv;
+package com.android.settings.turbo.dslv;
 
 import android.content.Context;
 import android.view.Gravity;
