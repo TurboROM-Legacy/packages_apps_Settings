df27596d3fbf47bdf9c400485c54a49db55b167f
MZO9400 <hamza.hameed00@yahoo.com>
2016-03-31 16:00:27 -0400

    Add hook for Kernel Adiutor into Settings dashboard

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -285,6 +285,7 @@ public class SettingsActivity extends Activity
             R.id.home_settings,
             R.id.dashboard,
             R.id.bitsyko_layers,
+	    R.id.kernel_adiutor,
 	    R.id.turbotweaks
     };
 
@@ -1344,6 +1345,16 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+                } else if (id == R.id.kernel_adiutor) {
+		         boolean supported = false;
+		         try {
+		             supported = (getPackageManager().getPackageInfo("com.grarak.kerneladiutor", 0).versionCode > 0);
+		    } catch (PackageManager.NameNotFoundException e) {
+
+		    }
+		    if (!supported) {
+			removeTile = true;
+		    }
                 } else if (id == R.id.google_settings) {
                     // Embedding into Settings is supported from Google Settings
                     boolean supported = false;
