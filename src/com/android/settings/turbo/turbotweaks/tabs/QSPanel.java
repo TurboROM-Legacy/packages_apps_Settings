6eb45e6cd1fc4cf7e51ac39e4ae1c015adb2b429
winga45 <winga45@gmail.com>
2016-04-02 21:55:59 -0400

    SETTINGS: Add new contextual header image settings
    * Also fix a double that wasn't making header shadows stick

--- src/com/android/settings/turbo/turbotweaks/tabs/QSPanel.java
+++ src/com/android/settings/turbo/turbotweaks/tabs/QSPanel.java
@@ -16,16 +16,30 @@
 
 package com.android.settings.turbo.turbotweaks.tabs;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
+import com.android.settings.Utils;
+import android.provider.SearchIndexableResource;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -38,21 +52,24 @@ import com.android.settings.turbo.widget.SeekBarPreferenceCham;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
+import java.util.List;
+import java.util.ArrayList;
 
 public class QSPanel extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "QSPanel";
 
     private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
-    private static final String PREF_CUSTOM_HEADER = "status_bar_custom_header";
-    private static final String PREF_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
     private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
+    private static final String CUSTOM_HEADER_IMAGE = "status_bar_custom_header";
+    private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
+    private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
 
     private ListPreference mNumColumns;
     private ListPreference mQuickPulldown;
-    private SwitchPreference mCustomHeader;
-    private SwitchPreference mCustomHeaderDefault;
     private SeekBarPreferenceCham mHeaderShadow;
+    private ListPreference mDaylightHeaderPack;
+    private SwitchPreference mCustomHeaderImage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,6 +85,39 @@ public class QSPanel extends SettingsPreferenceFragment implements
         PreferenceScreen prefSet = getPreferenceScreen();
  	ContentResolver resolver = getActivity().getContentResolver();
 
+	// header image packs
+        final boolean customHeaderImage = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1;
+        mCustomHeaderImage = (SwitchPreference) findPreference(CUSTOM_HEADER_IMAGE);
+        mCustomHeaderImage.setChecked(customHeaderImage);
+
+        String imageHeaderPackage = Settings.System.getString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
+        if (imageHeaderPackage == null) {
+		imageHeaderPackage = DEFAULT_HEADER_PACKAGE;
+        }
+
+	mDaylightHeaderPack = (ListPreference) findPreference(DAYLIGHT_HEADER_PACK);
+        List<String> entries = new ArrayList<String>();
+        List<String> values = new ArrayList<String>();
+        getAvailableHeaderPacks(entries, values);
+        mDaylightHeaderPack.setEntries(entries.toArray(new String[entries.size()]));
+        mDaylightHeaderPack.setEntryValues(values.toArray(new String[values.size()]));
+ 
+        int valueIndexHeader = mDaylightHeaderPack.findIndexOfValue(imageHeaderPackage);
+        if (valueIndexHeader == -1) {
+	    // no longer found
+            imageHeaderPackage = DEFAULT_HEADER_PACKAGE;
+            Settings.System.putString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, imageHeaderPackage);
+            valueIndexHeader = mDaylightHeaderPack.findIndexOfValue(imageHeaderPackage);
+        }
+
+        mDaylightHeaderPack.setValueIndex(valueIndexHeader >= 0 ? valueIndexHeader : 0);
+        mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntry());
+        mDaylightHeaderPack.setOnPreferenceChangeListener(this);
+        mDaylightHeaderPack.setEnabled(customHeaderImage);
+ 
         mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
         if (!Utils.isPhone(getActivity()) && !Utils.isTablet(getActivity())) {
             prefSet.removePreference(mQuickPulldown);
@@ -80,20 +130,10 @@ public class QSPanel extends SettingsPreferenceFragment implements
             updateQuickPulldownSummary(statusQuickPulldown);
         }
 
-        mCustomHeader = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER);
-        mCustomHeader.setChecked((Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1));
-        mCustomHeader.setOnPreferenceChangeListener(this);
-
-        mCustomHeaderDefault = (SwitchPreference) prefSet.findPreference(PREF_CUSTOM_HEADER_DEFAULT);
-        mCustomHeaderDefault.setChecked((Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT, 0) == 1));
-        mCustomHeaderDefault.setOnPreferenceChangeListener(this);
-
         mHeaderShadow = (SeekBarPreferenceCham) findPreference(CUSTOM_HEADER_IMAGE_SHADOW);
         final int headerShadow = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0);
-        mHeaderShadow.setValue((int)((headerShadow / 255) * 100));
+        mHeaderShadow.setValue((int)(((double) headerShadow / 255) * 100));
         mHeaderShadow.setOnPreferenceChangeListener(this);
 
         mNumColumns = (ListPreference) findPreference("sysui_qs_num_columns");
@@ -122,11 +162,6 @@ public class QSPanel extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getActivity().getContentResolver();
 	if (preference == mQuickPulldown) {
@@ -142,22 +177,54 @@ public class QSPanel extends SettingsPreferenceFragment implements
                     numColumns, UserHandle.USER_CURRENT);
             updateNumColumnsSummary(numColumns);
             return true;
-        } else if (preference == mCustomHeader) {
-            Settings.System.putInt(resolver, Settings.System.STATUS_BAR_CUSTOM_HEADER,
-                    (Boolean) objValue ? 1 : 0);
-            return true;
-        } else if (preference == mCustomHeaderDefault) {
-            Settings.System.putInt(resolver, Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT,
-                    (Boolean) objValue ? 1 : 0);
-            return true;
         } else if (preference == mHeaderShadow) {
-         Integer headerShadow = (Integer) objValue;
-         int realHeaderValue = (int) (((double) headerShadow / 100) * 255);
-         Settings.System.putInt(getContentResolver(),
-                 Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, realHeaderValue);
-            return true;
-        }
+	    Integer headerShadow = (Integer) objValue;
+	    int realHeaderValue = (int) (((double) headerShadow / 100) * 255);
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, realHeaderValue);
+	    return true;
+	} else if (preference == mDaylightHeaderPack) {
+	    String value = (String) objValue;
+	    Settings.System.putString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
+	    int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
+	    mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
+	    return true;
+	}
         return false;
+    }
+
+  private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.DaylightHeaderPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                values.add(0, packageName);
+            } else {
+                values.add(packageName);
+            }
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                entries.add(0, label);
+            } else {
+                entries.add(label);
+            }
+        }
+        i.setAction("org.omnirom.DaylightHeaderPack1");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            values.add(packageName  + "/" + r.activityInfo.name);
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = packageName;
+            }
+            entries.add(label);
+        }
     }
 
     private void updateNumColumnsSummary(int numColumns) {
@@ -179,8 +246,21 @@ public class QSPanel extends SettingsPreferenceFragment implements
         }
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+	if (preference == mCustomHeaderImage) {
+	    final boolean value = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getContentResolver(),
+		Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
+            mDaylightHeaderPack.setEnabled(value);
+            return true;
+	}
+	return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+  
+
     private void updateQuickPulldownSummary(int value) {
-        Resources res = getResources();
+       Resources res = getResources();
 
        if (value == 0) {
             // quick pulldown deactivated
