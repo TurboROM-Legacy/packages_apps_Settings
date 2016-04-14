e9cf4cc41b0677639c44f3c16c8ebe7026dedacf
winga45 <winga45@gmail.com>
2016-04-13 22:06:52 -0400

    Turbo status bar logo position (2/2)
    Brought to Turbo ROM by blinga6

--- src/com/android/settings/turbo/StatusBarLogoSettings.java
+++ src/com/android/settings/turbo/StatusBarLogoSettings.java
@@ -41,8 +41,10 @@ public class StatusBarLogoSettings extends SettingsPreferenceFragment implements
     private static final String TAG = "StatusBarLogoSettings";
 
     private static final String KEY_TURBO_LOGO_COLOR = "status_bar_turbo_logo_color";
+    private static final String KEY_TURBO_LOGO_STYLE = "status_bar_turbo_logo_style";
 
     private ColorPickerPreference mTurboLogoColor;
+    private ListPreference mTurboLogoStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,14 @@ public class StatusBarLogoSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.statusbar_logo);
         PreferenceScreen prefSet = getPreferenceScreen();
+		ContentResolver resolver = mContext.getContentResolver();
+         // Turbo logo Style
+	mTurboLogoStyle = (ListPreference) findPreference(KEY_TURBO_LOGO_STYLE);
+	    int turboLogoStyle = Settings.System.getIntForUser(resolver,
+	    Settings.System.STATUS_BAR_TURBO_LOGO_STYLE, 0, UserHandle.USER_CURRENT);
+	mTurboLogoStyle.setValue(String.valueOf(turboLogoStyle));
+	mTurboLogoStyle.setSummary(mTurboLogoStyle.getEntry());
+	mTurboLogoStyle.setOnPreferenceChangeListener(this);
 
         // Turbo logo color
 	mTurboLogoColor =
@@ -64,6 +74,7 @@ public class StatusBarLogoSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+	ContentResolver resolver = mContext.getContentResolver();
 	if (preference == mTurboLogoColor) {
             String hex = ColorPickerPreference.convertToARGB(
 		Integer.valueOf(String.valueOf(newValue)));
@@ -72,6 +83,14 @@ public class StatusBarLogoSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(),
 		Settings.System.STATUS_BAR_TURBO_LOGO_COLOR, intHex);
             return true;
+	} else if (preference == mTurboLogoStyle) {
+	    int turboLogoStyle = Integer.valueOf((String) newValue);
+	    int index = mTurboLogoStyle.findIndexOfValue((String) newValue);
+	    Settings.System.putIntForUser(resolver, 
+		Settings.System.STATUS_BAR_TURBO_LOGO_STYLE, turboLogoStyle, UserHandle.USER_CURRENT);
+	    mTurboLogoStyle.setSummary(
+	        mTurboLogoStyle.getEntries()[index]);
+	    return true;
         }
         return false;
     }
