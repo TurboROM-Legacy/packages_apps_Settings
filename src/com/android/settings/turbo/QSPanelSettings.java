bcb4f802fb9353b52937587e72dc21a86d38534f
Randall Rushing <randall.rushing@gmail.com>
2016-06-06 14:57:15 -0400

    Set number of tile columns [2/2]
    PureNexus Edits:
    * Make arrays point to strings for easy translatability
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/turbo/QSPanelSettings.java
+++ src/com/android/settings/turbo/QSPanelSettings.java
@@ -20,7 +20,9 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -41,19 +43,61 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class QSPanelSettings extends SettingsPreferenceFragment implements Indexable {
+public class QSPanelSettings extends SettingsPreferenceFragment implements 
+	OnPreferenceChangeListener, Indexable {
+
+    private static final String PREF_QS_COLUMNS = "sysui_qs_num_columns";
+
+    private ListPreference mNumColumns;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.qs_panel_settings);
 
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mNumColumns = (ListPreference) findPreference(PREF_QS_COLUMNS);
+        int numColumns = Settings.Secure.getIntForUser(resolver,
+            Settings.Secure.QS_NUM_TILE_COLUMNS, getDefaultNumColums(),
+            UserHandle.USER_CURRENT);
+        mNumColumns.setValue(String.valueOf(numColumns));
+        updateNumColumnsSummary(numColumns);
+        mNumColumns.setOnPreferenceChangeListener(this);
     }
     
     @Override
     protected int getMetricsCategory()
     {
 	return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNumColumns) {
+            int numColumns = Integer.valueOf((String) newValue);
+            Settings.Secure.putIntForUser(resolver, Settings.Secure.QS_NUM_TILE_COLUMNS,
+                numColumns, UserHandle.USER_CURRENT);
+            updateNumColumnsSummary(numColumns);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateNumColumnsSummary(int numColumns) {
+        String prefix = (String) mNumColumns.getEntries()[mNumColumns.findIndexOfValue(String.valueOf(numColumns))];
+        mNumColumns.setSummary(getActivity().getResources().getString(R.string.qs_num_columns_showing, prefix));
+    }
+
+    private int getDefaultNumColums() {
+        try {
+            Resources res = getActivity().getPackageManager().getResourcesForApplication("com.android.systemui");
+            int val = res.getInteger(res.getIdentifier("quick_settings_num_columns", "integer", "com.android.systemui")); 
+            // This better not be larger than 5, that's as high as the list goes atm
+            return Math.max(1, val);
+        } catch (Exception e) {
+            return 3;
+        }
     }
 }
