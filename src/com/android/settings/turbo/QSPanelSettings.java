f30a1c5abb28ceaaf7d8932af7f3800d9f0aa1e3
Raleigh Matlock <raleighman2@gmail.com>
2016-07-19 14:33:39 -0400

    Turbo settings: Create a QS category
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

---
+++ src/com/android/settings/turbo/QSPanelSettings.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 Turbo ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package com.android.settings.turbo;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class QSPanelSettings extends SettingsPreferenceFragment implements Indexable {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.qs_panel_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+    
+    @Override
+    protected int getMetricsCategory()
+    {
+	return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+}
