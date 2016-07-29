ResurrectionRemix/Resurrection_packages_apps_Settings

Settings: Allow launching display rotation settings externally
Change-Id: I69168fea2a43cfa7f2b8474042c35720f4431040

src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -364,6 +365,7 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
             LiveDisplay.class.getName(),
+            com.android.settings.cyanogenmod.DisplayRotation.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName()
     };

TurboROM-Legacy/packages_apps_Settings

Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -119,6 +119,7 @@ import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.turbo.MainSettings;
+import com.android.settings.turbo.DisplayRotation;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
@@ -362,6 +363,7 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
 	    MainSettings.class.getName(),
+            com.android.settings.turbo.DisplayRotation.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
 	    com.android.settings.privacyguard.PrivacyGuardManager.class.getName()
     };