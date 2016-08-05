cdb26f567eac254a10c096fb78e501b2942e75bc
Raleigh Matlock <raleighman2@gmail.com>
2016-08-05 18:13:26 -0400

    Add a color option to status bar carrier label (2/2)
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/turbo/StatusBarCarrierLabelSettings.java
+++ src/com/android/settings/turbo/StatusBarCarrierLabelSettings.java
@@ -16,19 +16,29 @@
 package com.android.settings.turbo;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.Spannable;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.android.internal.logging.MetricsLogger;
@@ -36,24 +46,55 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
 
+    private static final String TAG = "StatusBarCarrierLabelSettings";
+
     private static final String SHOW_CARRIER_LABEL = "status_bar_show_carrier";
+    private static final String CARRIER_COLOR_OVERRIDE = "status_bar_carrier_color_override";
+    private static final String CARRIER_COLOR_PICKER = "carrier_label_color";
     private static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
     private PreferenceScreen mCustomCarrierLabel;
+    private SwitchPreference mCarrierColorOverride;
+    private ColorPickerPreference mCarrierColorPicker;
     private ListPreference mShowCarrierLabel;
     private String mCustomCarrierLabelText;
+
+    private boolean mCheckPreferences;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createCustomView();
+    }
+
+    private PreferenceScreen createCustomView() {
+        mCheckPreferences = false;
+        PreferenceScreen prefSet = getPreferenceScreen();
+        if (prefSet != null) {
+            prefSet.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.status_bar_carrier_label_settings);
+        prefSet = getPreferenceScreen();
 
-        PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        PackageManager pm = getPackageManager();
+        Resources systemUiResources;
+        try {
+            systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            Log.e(TAG, "can't access systemui resources",e);
+            return null;
+        }
 
         mShowCarrierLabel = (ListPreference) findPreference(SHOW_CARRIER_LABEL);
         int showCarrierLabel = Settings.System.getInt(resolver,
@@ -61,6 +102,29 @@ public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
         mShowCarrierLabel.setValue(String.valueOf(showCarrierLabel));
         mShowCarrierLabel.setSummary(mShowCarrierLabel.getEntry());
         mShowCarrierLabel.setOnPreferenceChangeListener(this);
+
+        mCarrierColorOverride =
+                (SwitchPreference) prefSet.findPreference(CARRIER_COLOR_OVERRIDE);
+        mCarrierColorOverride.setChecked((Settings.System.getInt(
+                getActivity().getApplicationContext().getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_COLOR_OVERRIDE, 0) == 1));
+        mCarrierColorOverride.setOnPreferenceChangeListener(this);
+
+        mCarrierColorPicker = (ColorPickerPreference) findPreference(CARRIER_COLOR_PICKER);
+        mCarrierColorPicker.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_CARRIER_COLOR, -2);
+        if (intColor == -2) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/status_bar_carrier_color", null, null));
+            mCarrierColorPicker.setSummary(getResources().getString(
+                    R.string.default_string));
+        } else {
+            String hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mCarrierColorPicker.setSummary(hexColor);
+        }
+        mCarrierColorPicker.setNewPreviewColor(intColor);
+
         mCustomCarrierLabel = (PreferenceScreen) findPreference(CUSTOM_CARRIER_LABEL);
 
         if (TelephonyManager.getDefault().isMultiSimEnabled()) {
@@ -68,11 +132,10 @@ public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
         } else {
             updateCustomLabelTextSummary();
         }
-    }
 
-    @Override
-    protected int getMetricsCategory() {
-        return MetricsLogger.DONT_TRACK_ME_BRO;
+        setHasOptionsMenu(true);
+        mCheckPreferences = true;
+        return prefSet;
     }
 
     private void updateCustomLabelTextSummary() {
@@ -87,6 +150,10 @@ public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!mCheckPreferences) {
+            return false;
+        }
+        AlertDialog dialog;
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mShowCarrierLabel) {
             int showCarrierLabel = Integer.valueOf((String) newValue);
@@ -95,8 +162,44 @@ public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
                 STATUS_BAR_SHOW_CARRIER, showCarrierLabel);
             mShowCarrierLabel.setSummary(mShowCarrierLabel.getEntries()[index]);
             return true;
+        } else if (preference == mCarrierColorOverride) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.STATUS_BAR_CARRIER_COLOR_OVERRIDE,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mCarrierColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_CARRIER_COLOR, intHex);
+            return true;
          }
          return false;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_settings_reset)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
     }
 
     @Override
@@ -128,5 +231,53 @@ public class StatusBarCarrierLabelSettings extends SettingsPreferenceFragment
             alert.show();
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        StatusBarCarrierLabelSettings getOwner() {
+            return (StatusBarCarrierLabelSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.carrier_label_color_reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getActivity().getContentResolver(),
+                                Settings.System.STATUS_BAR_CARRIER_COLOR, -2);
+                            getOwner().createCustomView();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 }
