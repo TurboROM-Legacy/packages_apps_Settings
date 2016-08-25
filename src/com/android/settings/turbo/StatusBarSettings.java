e8c070b2c3c9f95681a1248ca0046e5426f81e72
maxwen <max.weninger@gmail.com>
2016-08-25 16:21:43 -0400

    [SQUASH] Add contextual header settings
    This includes:
    * Default headers
    * Omni headers [Nature, nature (random)]
    * Poly HD headers
    * Omni's header shadow configuration
    
    Authorship goes to maxwen as he did most of the header tweaking
    
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/turbo/StatusBarSettings.java
+++ src/com/android/settings/turbo/StatusBarSettings.java
@@ -19,11 +19,16 @@ package com.android.settings.turbo;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.CustomSeekBarPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
@@ -50,8 +55,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 6;
 
+    private static final String CUSTOM_HEADER_IMAGE_SHADOW = "status_bar_custom_header_shadow";
+    private static final String CUSTOM_HEADER_IMAGE = "status_bar_custom_header";
+    private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
+    private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
+
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarBatteryShowPercent;
+
+    private CustomSeekBarPreference mHeaderShadow;
+    private ListPreference mDaylightHeaderPack;
+    private SwitchPreference mCustomHeaderImage;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -76,6 +90,46 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
         enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
+
+	// Header image packs
+        final boolean customHeaderImage = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1;
+        mCustomHeaderImage = (SwitchPreference) findPreference(CUSTOM_HEADER_IMAGE);
+        mCustomHeaderImage.setChecked(customHeaderImage);
+
+        String imageHeaderPackage = Settings.System.getString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
+        if (imageHeaderPackage == null) {
+		imageHeaderPackage = DEFAULT_HEADER_PACKAGE;
+        }
+
+	mDaylightHeaderPack = (ListPreference) findPreference(DAYLIGHT_HEADER_PACK);
+        List<String> entries = new ArrayList<String>();
+        List<String> values = new ArrayList<String>();
+        getAvailableHeaderPacks(entries, values);
+        mDaylightHeaderPack.setEntries(entries.toArray(new String[entries.size()]));
+        mDaylightHeaderPack.setEntryValues(values.toArray(new String[values.size()]));
+ 
+        int valueIndexHeader = mDaylightHeaderPack.findIndexOfValue(imageHeaderPackage);
+        if (valueIndexHeader == -1) {
+	    // no longer found
+            imageHeaderPackage = DEFAULT_HEADER_PACKAGE;
+            Settings.System.putString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, imageHeaderPackage);
+            valueIndexHeader = mDaylightHeaderPack.findIndexOfValue(imageHeaderPackage);
+        }
+
+        mDaylightHeaderPack.setValueIndex(valueIndexHeader >= 0 ? valueIndexHeader : 0);
+        mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntry());
+        mDaylightHeaderPack.setOnPreferenceChangeListener(this);
+        mDaylightHeaderPack.setEnabled(customHeaderImage);
+
+        // Header shadow
+        mHeaderShadow = (CustomSeekBarPreference) findPreference(CUSTOM_HEADER_IMAGE_SHADOW);
+        final int headerShadow = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, 0);
+        mHeaderShadow.setValue((int)(((double) headerShadow / 255) * 100));
+        mHeaderShadow.setOnPreferenceChangeListener(this);
     }
     
     @Override
@@ -103,6 +157,19 @@ public class StatusBarSettings extends SettingsPreferenceFragment
             mStatusBarBatteryShowPercent.setSummary(
                     mStatusBarBatteryShowPercent.getEntries()[index]);
             return true;
+        } else if (preference == mHeaderShadow) {
+	    Integer headerShadow = (Integer) newValue;
+	    int realHeaderValue = (int) (((double) headerShadow / 100) * 255);
+	    Settings.System.putInt(getContentResolver(),
+		Settings.System.STATUS_BAR_CUSTOM_HEADER_SHADOW, realHeaderValue);
+	    return true;
+	} else if (preference == mDaylightHeaderPack) {
+	    String value = (String) newValue;
+	    Settings.System.putString(getContentResolver(),
+		Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
+	    int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
+	    mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
+	    return true;
         }
         return false;
     }
@@ -114,5 +181,50 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         } else {
             mStatusBarBatteryShowPercent.setEnabled(true);
         }
+    }
+
+  private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.DaylightHeaderPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                values.add(0, packageName);
+            } else {
+                values.add(packageName);
+            }
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                entries.add(0, label);
+            } else {
+                entries.add(label);
+            }
+        }
+        i.setAction("org.omnirom.DaylightHeaderPack1");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            values.add(packageName  + "/" + r.activityInfo.name);
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = packageName;
+            }
+            entries.add(label);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+	if (preference == mCustomHeaderImage) {
+	    final boolean value = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
+            mDaylightHeaderPack.setEnabled(value);
+            return true;
+	}
+	return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
