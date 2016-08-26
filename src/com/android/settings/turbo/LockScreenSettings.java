87e0d888c7e7a2634b8a47437e3c2022ee2ff9fc
Raleigh Matlock <raleighman2@gmail.com>
2016-08-26 15:16:15 -0400

    [SQUASH] Settings: Add lock screen weather settings [2/2]
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- src/com/android/settings/turbo/LockScreenSettings.java
+++ src/com/android/settings/turbo/LockScreenSettings.java
@@ -16,6 +16,12 @@
 
 package com.android.settings.turbo;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
@@ -27,22 +33,48 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.view.IWindowManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 public class LockScreenSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEYGUARD_CLOCK_FONT = "keyguard_clock_font";
+    private static final String PREF_SHOW_WEATHER = "weather_show_weather";
+    private static final String PREF_SHOW_LOCATION = "weather_show_location";
+    private static final String PREF_CONDITION_ICON = "weather_condition_icon";
+    private static final String PREF_TEXT_COLOR = "weather_text_color";
+    private static final String PREF_ICON_COLOR = "weather_icon_color";
+
+    private static final int MONOCHROME_ICON = 0;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
 
     private ListPreference mKeyguardClockFont;
+    private SwitchPreference mShowWeather;
+    private SwitchPreference mShowLocation;
+    private ListPreference mConditionIcon;
+    private ColorPickerPreference mTextColor;
+    private ColorPickerPreference mIconColor;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
 
+    public void refreshSettings() {
         PreferenceScreen prefSet = getPreferenceScreen();
         if (prefSet != null) {
             prefSet.removeAll();
@@ -51,12 +83,91 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements On
         addPreferencesFromResource(R.xml.lock_screen_settings);
         prefSet = getPreferenceScreen();
 
+        mResolver = getActivity().getContentResolver();
+
         mKeyguardClockFont = (ListPreference) findPreference(KEYGUARD_CLOCK_FONT);
         mKeyguardClockFont.setValue(String.valueOf(Settings.System.getInt(
                 getContentResolver(), Settings.System.LOCK_SCREEN_CLOCK_FONT, 4)));
         mKeyguardClockFont.setSummary(mKeyguardClockFont.getEntry());
         mKeyguardClockFont.setOnPreferenceChangeListener(this);
+
+        boolean showWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+
+        int intColor;
+        String hexColor;
+
+        mShowWeather = (SwitchPreference) findPreference(PREF_SHOW_WEATHER);
+        mShowWeather.setChecked(showWeather);
+        mShowWeather.setOnPreferenceChangeListener(this);
+
+        mTextColor = (ColorPickerPreference) findPreference(PREF_TEXT_COLOR);
+        mIconColor = (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
+
+        if (showWeather) {
+            mShowLocation = (SwitchPreference) findPreference(PREF_SHOW_LOCATION);
+            mShowLocation.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1) == 1);
+            mShowLocation.setOnPreferenceChangeListener(this);
+
+            mConditionIcon = (ListPreference) findPreference(PREF_CONDITION_ICON);
+            mConditionIcon.setValue(String.valueOf(conditionIcon));
+            mConditionIcon.setSummary(mConditionIcon.getEntry());
+            mConditionIcon.setOnPreferenceChangeListener(this);
+
+            intColor = Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, -2);
+            if (intColor == -2) {
+                intColor = 0xffffffff;
+                mTextColor.setSummary(getResources().getString(R.string.color_default));
+            } else {
+                hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mTextColor.setSummary(hexColor);
+            }
+            mTextColor.setNewPreviewColor(intColor);
+            mTextColor.setOnPreferenceChangeListener(this);
+
+            intColor = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, -2);
+            if (intColor == -2) {
+                intColor = 0xffffffff;
+                mIconColor.setSummary(getResources().getString(R.string.color_default));
+            } else {
+                hexColor = String.format("#%08x", (0xffffffff & intColor));
+                mIconColor.setSummary(hexColor);
+            }
+            mIconColor.setNewPreviewColor(intColor);
+            mIconColor.setOnPreferenceChangeListener(this);
+        } else {
+            removePreference(PREF_SHOW_LOCATION);
+            removePreference(PREF_CONDITION_ICON);
+            removePreference(PREF_ICON_COLOR);
+            removePreference(PREF_TEXT_COLOR);
+        }
+
+        setHasOptionsMenu(true);
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(com.android.internal.R.drawable.ic_menu_refresh)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 
     @Override
     protected int getMetricsCategory() {
@@ -65,7 +176,48 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements On
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mKeyguardClockFont) {
+        boolean value;
+        String hex;
+        int intHex;
+
+        if (preference == mShowWeather) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER,
+                    value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mShowLocation) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mConditionIcon) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mConditionIcon.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
+            mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
+            refreshSettings();
+            return true;
+        } else if (preference == mTextColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mIconColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mKeyguardClockFont) {
             Settings.System.putInt(getContentResolver(), Settings.System.LOCK_SCREEN_CLOCK_FONT,
                     Integer.valueOf((String) newValue));
             mKeyguardClockFont.setValue(String.valueOf(newValue));
@@ -78,5 +230,62 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements On
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        LockScreenSettings getOwner() {
+            return (LockScreenSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.reset_values_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.reset_lock_screen_weather,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, -2);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, -2);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, null)
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 }
